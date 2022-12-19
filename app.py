e446b442f4b5e22a9f239e37dadb2aede4db5903
Paul <24rawson24@gmail.com>
2022-12-19 00:10:04 -0600

    Mad libs commit

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-num1 = input("Enter a number: ")
-num2 = input("Enter another number: ")
-result = float(num1) + float(num2)
+color = input("Enter a color: ")
+plural_noun = input("Enter a Plural Noun: ")
+celebrity = input("Enter a Celebrity: ")
 
-print(result)
+
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)
